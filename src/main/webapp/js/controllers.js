f677d879be5c7cfc2cf612a03a0832f90c0ecdd0
Shevtsou <shevtsou@gmail.com>
2017-02-06 20:02:11 +0300

    Angular removed.

--- src/main/webapp/js/controllers.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('jsender.controllers', []);
-
-app.controller('SettingController', ['$scope', 'SettingFactory', function ($scope, SettingFactory) {
-    SettingFactory.get({}, function (settingFactory) {
-        $scope.settingName = settingFactory.name;
-    })
-}]);
