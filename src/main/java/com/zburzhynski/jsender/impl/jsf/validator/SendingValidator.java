272552dee29c7fcf75a663ca726cb1b09f46ca9b
zburzhynski <zburzhynski@gmail.com>
2017-03-11 23:49:38 +0300

    Refined sending validator.

--- src/main/java/com/zburzhynski/jsender/impl/jsf/validator/SendingValidator.java
+++ src/main/java/com/zburzhynski/jsender/impl/jsf/validator/SendingValidator.java
@@ -20,11 +20,11 @@ public class SendingValidator extends BaseValidator {
 
     private static final String SENDING_ACCOUNT_NOT_SPECIFIED = "sendingValidator.sendingAccountNotSpecified";
 
-    private static final String FROM_NOT_SPECIFIED = "sendingValidator.fromNotSpecified";
-
     private static final String SUBJECT_NOT_SPECIFIED = "sendingValidator.subjectNotSpecified";
 
     private static final String TEXT_NOT_SPECIFIED = "sendingValidator.textNotSpecified";
+
+    private static final String FROM_NOT_SPECIFIED = "sendingValidator.fromNotSpecified";
 
     private static final String RECIPIENTS_NOT_SPECIFIED = "sendingValidator.recipientsNotSpecified";
 
@@ -51,16 +51,16 @@ public class SendingValidator extends BaseValidator {
             addMessage(SENDING_ACCOUNT_NOT_SPECIFIED);
             return false;
         }
-        if (StringUtils.isBlank(message.getFrom())) {
-            addMessage(FROM_NOT_SPECIFIED);
-            return false;
-        }
         if (SendingType.EMAIL.equals(message.getSendingType()) && StringUtils.isBlank(message.getSubject())) {
             addMessage(SUBJECT_NOT_SPECIFIED);
             return false;
         }
         if (StringUtils.isBlank(message.getText())) {
             addMessage(TEXT_NOT_SPECIFIED);
+            return false;
+        }
+        if (StringUtils.isBlank(message.getFrom())) {
+            addMessage(FROM_NOT_SPECIFIED);
             return false;
         }
         if (CollectionUtils.isEmpty(message.getRecipients())) {
