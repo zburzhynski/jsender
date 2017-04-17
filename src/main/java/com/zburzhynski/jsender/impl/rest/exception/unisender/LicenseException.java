af74a0f6d662d4009cd04267c8d1984db9d170b4
Shevtsou <shevtsou@gmail.com>
2017-04-17 15:15:28 +0300

    https://bitbucket.org/zburzhynski/jsender/issues/41/ License setting added.

---
+++ src/main/java/com/zburzhynski/jsender/impl/rest/exception/unisender/LicenseException.java
@@ -0,0 +1,11 @@
+package com.zburzhynski.jsender.impl.rest.exception.unisender;
+
+/**
+ * License excpeiton.
+ * <p/>
+ * Date: 17.04.2017
+ *
+ * @author Nikita Shevtsou
+ */
+public class LicenseException extends UnisenderException {
+}
